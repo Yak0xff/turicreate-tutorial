31fb3ea629a06d8fe439f50b09630483f69ddef1
Robin <1933549736@qq.com>
2018-01-23 14:54:53 +0800

    add data download shell

---
+++ Activity classifier/data_downloader.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+set -e
+# Download HAPT Data.
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/00341/HAPT%20Data%20Set.zip
+unzip HAPT%20Data%20Set.zip -d HAPT Data Set
+chmod +x HAPT Data Set
+
